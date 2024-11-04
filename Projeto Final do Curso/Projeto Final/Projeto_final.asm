6a1bc6112bfe7c5d9bcbd217b467807782fedd5d
Luca Francesco Filippi <luca.ffilippi@gmail.com>
2024-11-04 13:29:09 -0300

    aula 11

--- Projeto Final do Curso/Projeto Final/Projeto_final.asm
+++ Projeto Final do Curso/Projeto Final/Projeto_final.asm
@@ -2,15 +2,8 @@ TITLE Batalha Naval
 .MODEL SMALL
 .STACK 100h ;determina o tamanho do pilha
 .DATA
-    DESISTIR DB "Voce desistiu do jogo, obrigado por jogar! $"
-    MSG1 DB "Este eh o tabuleiro do jogo: $"
-    VITORIA DB "Parabens voce venceu!! $"
-    ACERTO DB "Voce acertou uma embarcacao inimiga! Continue tentando. $"
-    BOMBABARCO DB "X $"
-    ERRO DB "Voce errou, tente novamente! $"
-    BOMBAERRO DB "?$"
     TABULEIRO DW 20 DUP(20 DUP('*'))
-
+    JOGADAS DB 
 
 
 .CODE 
